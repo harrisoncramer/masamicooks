0cbb08dbd48f3dcbc8d1ae24f6fc8397582019d6
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-14 20:04:46 -0500

    Gutted old css files.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,29 +1,28 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import * as styles from './navigation.module.css'
 import Search from '../search'
 
 const Navigation = () => (
-  <nav role="navigation" className={styles.container} aria-label="Main">
-    <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
-      <span className={styles.navigationItem}>masamicooks</span>
+  <nav role="navigation" aria-label="Main">
+    <Link to="/" >
+      <span />
+      <span>masamicooks</span>
     </Link>
-    <div className={styles.containerRight}>
+    <div>
       <Search />
-      <ul className={styles.navigation}>
-        <li className={styles.navigationItem}>
+      <ul>
+        <li>
           <Link to="/" activeClassName="active">
             Home
           </Link>
         </li>
-        <li className={styles.navigationItem}>
+        <li>
           <Link to="/recipe/" activeClassName="active">
             Recipes
           </Link>
         </li>
-        <li className={styles.navigationItem}>
+        <li>
           <Link to="/blog/" activeClassName="active">
             Blog
           </Link>
