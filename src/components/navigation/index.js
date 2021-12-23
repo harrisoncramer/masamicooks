f75ca3f09981f7c3cc8096710c6f43fed1270733
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-22 21:54:30 -0500

    Added instagram logo.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from "../../assets/hot-pot.png"
+import logo from '../../assets/hot-pot.png'
+import insta from '../../assets/instagram.png'
+// import youtube from '../../assets/yt.png'
 
 const Navigation = () => (
-  <nav role="navigation" aria-label="Main" className="flex justify-between nav px-3 md:px-2 py-2 lg:px-3 lg:py-3 fixed top-0 bg-app-white w-full h-18 drop-shadow z-20">
-    <div className="nav__left">
-      <Link className="flex justify-start items-center pl-2" to="/" >
+  <nav
+    role="navigation"
+    aria-label="Main"
+    className="flex justify-between nav px-3 md:px-2 py-2 lg:px-5 lg:py-3 fixed top-0 bg-app-white w-full h-18 drop-shadow z-20"
+  >
+    <div className="flex gap-2 lg:gap-4 justify-start items-center nav__right flex-grow basis-0">
+      <Link to="/about" activeClassName="active">
+        <h2 className="font-display sm:text-lg md:text-lg lg:text-xl">About</h2>
+      </Link>
+      <Link to="/recipe/" activeClassName="active">
+        <h2 className="font-display sm:text-lg md:text-lg lg:text-xl">
+          Recipes
+        </h2>
+      </Link>
+      <Link to="/blog/" activeClassName="active">
+        <h2 className="font-display sm:text-lg md:text-lg lg:text-xl">Blog</h2>
+      </Link>
+    </div>
+    <div>
+      <Link className="flex justify-start items-center pl-2" to="/">
         <div className="object-scale-down h-8 w-8 lg:h-10 lg:w-10 mr-2">
           <img src={logo} alt="Logo" />
         </div>
@@ -14,22 +33,10 @@ const Navigation = () => (
         </div>
       </Link>
     </div>
-    <div className="flex gap-2 lg:gap-4 justify-end items-center nav__right">
-      <Link to="/about" activeClassName="active">
-        <h2 className="font-display sm:text-lg md:text-lg lg:text-xl">
-          About
-        </h2>
-      </Link>
-      <Link to="/recipe/" activeClassName="active">
-        <h2 className="font-display sm:text-lg md:text-lg lg:text-xl">
-          Recipes
-        </h2>
-      </Link>
-      <Link to="/blog/" activeClassName="active">
-        <h2 className="font-display sm:text-lg md:text-lg lg:text-xl">
-          Blog
-        </h2>
-      </Link>
+    <div className="flex align-center justify-end flex-grow basis-0">
+      <a href="https://instagram.com/masamicooks">
+        <img className="w-8 h-8" src={insta} alt="Instagram" />
+      </a>
     </div>
   </nav>
 )
