972cd49d7e39c877ce179de824fbdec6da835c77
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-27 12:30:07 -0500

    Fixed navbar for mobile.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,50 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import logo from '../../assets/hot-pot.png'
 import insta from '../../assets/insta.svg'
-// import youtube from '../../assets/yt.png'
 
-const Navigation = () => (
-  <nav
-    role="navigation"
-    aria-label="Main"
-    className="flex justify-between nav px-4 md:px-4 lg:px-6 py-2 lg:py-3 fixed top-0 w-full h-24 z-20 text-app-theme bg-app-white border-app-theme border-b"
-  >
-    <div className="flex gap-2 lg:gap-4 justify-start items-center nav__right flex-grow basis-0">
-      <Link to="/about" activeClassName="active">
-        <h2 className="font-display text-xl lg:text-2xl">About</h2>
-      </Link>
-      <Link to="/recipe/" activeClassName="active">
-        <h2 className="font-display text-xl lg:text-2xl">Recipes</h2>
-      </Link>
-      <Link to="/blog/" activeClassName="active">
-        <h2 className="font-display text-xl lg:text-2xl">Blog</h2>
-      </Link>
-    </div>
-    <div className="flex align-center justify-center">
-      <Link className="flex justify-start items-center pl-2" to="/">
-        <div className="object-scale-down h-8 w-8 lg:h-10 lg:w-10 ">
-          <img src={logo} />
-        </div>
-      </Link>
-    </div>
-    <div className="flex justify-end flex-grow basis-0 items-center">
-      <svg>
-        <a
-          href="https://instagram.com/masamicooks"
-          className="flex-grow basis-0 flex justify-end"
+const Navigation = () => {
+  const [isDropped, setIsDropped] = useState(false)
+
+  const handleDropdown = (e) => {
+    setIsDropped(!isDropped)
+  }
+
+  return (
+    <nav
+      role="navigation"
+      aria-label="Main"
+      className="flex justify-between items-center nav px-4 md:px-4 lg:px-6 py-2 lg:py-3 fixed top-0 w-full h-24 z-20 text-app-theme bg-app-white border-app-theme border-b"
+    >
+      <div className="absolute top-6 bg-app-white flex flex-col items-start gap-2 justify-start flex-grow basis-0 md:top-0 md:flex-row md:relative md:items-center lg:gap-4">
+        <button className="md:hidden" onClick={handleDropdown}>
+          <svg fill="#d00000" viewBox="0 -25 100 80" width="40" height="40">
+            <rect width="50" height="10"></rect>
+            <rect y="20" width="50" height="10"></rect>
+            <rect y="40" width="50" height="10"></rect>
+          </svg>
+        </button>
+        <div
+          className={'px-4 overflow-hidden transition-height duration-25 '.concat(
+            !isDropped
+              ? 'h-0 md:h-auto md:flex md:gap-2'
+              : 'h-28 py-2 border-app-theme border'
+          )}
         >
-          <image
-            width={25}
-            x={'70%'}
-            y={62}
-            xlinkHref={insta}
-            alt="Instagram"
-          />
-        </a>
-      </svg>
-    </div>
-  </nav>
-)
+          <Link to="/about" activeClassName="active">
+            <h2 className="font-display text-xl lg:text-2xl">About</h2>
+          </Link>
+          <Link to="/recipe/" activeClassName="active">
+            <h2 className="font-display text-xl lg:text-2xl">Recipes</h2>
+          </Link>
+          <Link to="/blog/" activeClassName="active">
+            <h2 className="font-display text-xl lg:text-2xl">Blog</h2>
+          </Link>
+        </div>
+      </div>
+      <div className="flex align-center justify-end flex-grow md:flex-grow-0">
+        <Link className="flex justify-start items-center pl-2" to="/">
+          <div className="object-scale-down h-8 w-8 lg:h-10 lg:w-10 ">
+            <img src={logo} />
+          </div>
+        </Link>
+      </div>
+      <div className="flex justify-end flex-grow basis-0 items-center">
+        <svg className="w-6">
+          <a
+            href="https://instagram.com/masamicooks"
+            className="flex-grow basis-0 flex justify-end"
+          >
+            <image width={25} y={62} xlinkHref={insta} alt="Instagram" />
+          </a>
+        </svg>
+      </div>
+    </nav>
+  )
+}
 
 export default Navigation
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,9 @@ module.exports = {
       screens: {
         betterhover: { raw: '(hover: hover)' },
       },
+      transitionProperty: {
+        height: 'height',
+      },
       boxShadow: {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
