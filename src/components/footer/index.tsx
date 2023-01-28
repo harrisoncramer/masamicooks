b1a0b102d4f15fc4889787717c5e8b89c06e955e
Harrison Cramer <harrisoncramer@gmail.com>
2023-01-28 00:24:31 -0500

    Migrated from Gatsby to Astro

--- src/components/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
-  <footer className="footer font-display bg-app-header">
-    <p>©2021 masamicooks.com</p>
-  </footer>
-)
-
-export default Footer
--- src/templates/blog.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BlogTemplate = function () {
-  return (
-    <h1>This blog page is still under construction!</h1>
-  )
-}
-
-export default BlogTemplate
